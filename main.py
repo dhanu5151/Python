b3c464dc0da4a19f75948d22007caacc36657421
Dhanashree <144604884+dhanu5151@users.noreply.github.com>
2023-09-15 15:03:39 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+p = input("enter value for principle")
+n = input("enter value for number of years")
+r = input("enter value for rate of interest")
+
+p = int(p)
+n = int(n)
+r = int(r)
+
+si = (p*n*r)/100
+
+print(si)
+
