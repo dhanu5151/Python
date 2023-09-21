6db782eb6bc13961c3ef96cf6db15aceace26de2
Dhanashree <144604884+dhanu5151@users.noreply.github.com>
2023-09-21 22:22:04 +0530

    Add files via upload

---
+++ OperationsOnLists.py
@@ -0,0 +1,15 @@
+# append function
+fruits =["Apple", "Mango", "Peach", "Orange"]
+print(fruits)
+fruits.append("Banana")
+print(fruits)
+# find out the length function
+print(len(fruits))
+# Insert function to add iteam at particular position
+fruits.insert(1,"Pomogranate")
+print(fruits)
+fruits.insert(0, "Pineapple")
+print(fruits)
+# find the position in list
+print(fruits.index("Mango"))
+
